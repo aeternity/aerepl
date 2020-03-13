54e1a3b864c9c0f4848af97e737af22973b43aca
radrow <radrowicki@gmail.com>
2020-03-13 15:56:13 +0100

    Updated tests

--- test/aere_tests.erl
+++ test/aere_tests.erl
@@ -14,50 +14,40 @@ run_test(Test) ->
     ok.
 
 eval_test_() ->
-    [{"Testing the " ++ TestScenario ++ " sample input",
+    [{"Testing the " ++ atom_to_list(TestScenario) ++ " scenario",
       fun() ->
-              {ok, Input} = file:read_file("test/inputs/" ++ TestScenario ++ ".aesi"),
-              Results = eval(binary_to_list(Input)),
-              ?assertEqual(Answers, Results)
-      end} || {TestScenario, Answers} <- evaluable_inputs()].
+              File = "test/scenarios/" ++ atom_to_list(TestScenario) ++ ".aesi",
+              Input = case file:read_file(File) of
+                          {ok, I} -> I;
+                          {error, enoent} -> error(File ++ " not found")
+                      end,
 
-evaluable_inputs() ->
-    [ { "twoplustwo"
-      , ["4"]
-      }
-    , { "twocommands"
-      , ["4", "6"]
-      }
-    , { "multiline"
-      , [ "4"
-        , "8"
-        , "\"B\""
-        ]
-      }
-    , { "letdef"
-      , [ "2"
-        , "2"
-        , "2"
-        , "\"XDDD\""
-        , "2"
-        , "7"
-        ]
-      }
-    , { "fundef"
-      , [ "3"
-        , "5"
-        , "100"
-        , "5"
-        , "true"
-        , "[2, 3, 4, 5, 6, 7]"
-        , "[2, 3, 4, 5, 6, 7]"
-        , "100000"
-        ]
-    }
+              {Answers, Results} = eval(binary_to_list(Input)),
+
+              %% FIXME better error msg
+              ?assertEqual(length(Answers), length(Results)),
+              Results1 =
+                  [ case A of
+                        any -> any;
+                        _ -> R
+                    end
+                    || {A, R} <- lists:zip(Answers, Results)
+                  ],
+              ?assertEqual(Answers, Results1)
+      end} || TestScenario <- scenarios()].
+
+scenarios() ->
+    [ my_test
+    , twoplustwo
+    , twocommands
+    , multiline
+    , letdef
+    , fundef
+    , datatypes
     ].
 
-uncolor(T) ->
-    aere_color:render_colored(none, T).
+format(T) ->
+    string:trim(aere_color:render_colored(none, T)).
 
 eval(I) ->
     InitSt =
@@ -68,22 +58,38 @@ eval(I) ->
     Splitted = aere_parse:split_input(I),
     Build =
         lists:foldr(
-          fun(Cmd, Cont) ->
-                  fun({S, AccL}) ->
+          fun("", Cont) -> Cont;
+             ([$/,$/|Ans], Cont) ->
+                  fun({S, {Answers, Inputs}}) ->
+                          case string:trim(Ans) of
+                              "_" -> Cont({S, {[any|Answers], Inputs}});
+                              "!error" -> Cont({S, {[error|Answers], Inputs}});
+                              A -> Cont({S, {[A|Answers], Inputs}})
+                          end
+                  end;
+             (Cmd, Cont) ->
+                  fun({S, {Answers, Inputs}}) ->
                           Resp = aerepl:process_string(S, Cmd),
                           case Resp of
                               #repl_response{status = finito} ->
-                                  lists:reverse(AccL);
+                                  {lists:reverse(Answers), lists:reverse(Inputs)};
                               #repl_response{status = {success, S1},
                                              output = ""
                                             } ->
-                                  Cont({S1, AccL});
+                                  Cont({S1, {Answers, Inputs}});
                               #repl_response{status = {success, S1},
                                              output = Out
                                             } ->
-                                  Cont({S1, [uncolor(Out)|AccL]})
+                                  Cont({S1, {Answers, [format(Out)|Inputs]}});
+                              #repl_response{status = error} ->
+                                  Cont({S, {Answers, [error|Inputs]}});
+                              #repl_response{status = internal_error,
+                                             output = Out
+                                            } ->
+                                  io:format(format(Out)),
+                                  error(internal_error)
                           end
                   end
-          end, fun({_, L}) -> lists:reverse(L) end, Splitted),
-    Outputs = Build({InitSt, []}),
+          end, fun({_, {As, Is}}) -> {lists:reverse(As), lists:reverse(Is)} end, Splitted),
+    Outputs = Build({InitSt, {[], []}}),
     Outputs.
