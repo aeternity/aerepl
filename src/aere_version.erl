e23f1b4a40cb91fa0bba8a1f63b4e01fe8f43652
Gaith Hallak <gaithhallak@gmail.com>
2022-01-27 18:46:39 +0400

    Use VM_FATE_SOPHIA_2 for IRIS

--- src/aere_version.erl
+++ src/aere_version.erl
@@ -15,5 +15,5 @@
 protocol_version() -> ?IRIS_PROTOCOL_VSN.
 sophia_version()   -> ?SOPHIA_IRIS_FATE.
 abi_version()      -> ?ABI_FATE_SOPHIA_1.
-vm_version()       -> ?VM_FATE_SOPHIA_1.
+vm_version()       -> ?VM_FATE_SOPHIA_2.
 contract_version() -> ?SOPHIA_CONTRACT_VSN_3.
