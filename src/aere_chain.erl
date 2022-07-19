a1c4c4e7e6c205f46f2b0f885b018ae60a0a8aa1
radrow <radrowicki@gmail.com>
2022-07-19 14:07:12 +0200

    Cleanup in aere_chain

--- src/aere_chain.erl
+++ src/aere_chain.erl
@@ -3,37 +3,28 @@
 -include("../node/apps/aecontract/include/aecontract.hrl").
 -include("../node/apps/aecore/include/blocks.hrl").
 
--export([new_account/2, default_tx_env/1, update_balance/3]).
+-export([init_trees/0, new_account/2, update_balance/3, default_tx_env/1]).
 
-new_account(Balance, Trees1) ->
-    {PubKey, PrivKey} = new_key_pair(),
-    Trees2            = set_account(aec_accounts:new(PubKey, Balance), Trees1),
-    {PubKey, Trees2}.
 
-update_balance(NewBalance, PubKey, State) ->
-    Trees = trees(State),
+init_trees() ->
+    aec_trees:new().
+
+new_account(Balance, Trees) ->
+    #{public := PubKey} = enacl:sign_keypair(),
+    Trees1      = set_account(aec_accounts:new(PubKey, Balance), Trees),
+    {PubKey, Trees1}.
+
+update_balance(NewBalance, PubKey, Trees) ->
     Account = aec_accounts_trees:get(PubKey, aec_trees:accounts(Trees)),
     OldBalance = aec_accounts:balance(Account),
     Nonce = aec_accounts:nonce(Account),
     {ok, Account1} = aec_accounts:spend(Account, OldBalance, Nonce + 1),
     {ok, Account2} = aec_accounts:earn(Account1, NewBalance),
-    set_account(Account2, State).
-
-new_key_pair() ->
-    #{ public := PubKey, secret := PrivKey } = enacl:sign_keypair(),
-    {PubKey, PrivKey}.
-
-insert_key_pair(Pub, Priv, S) ->
-    Old = key_pairs(S),
-    S#{key_pairs => Old#{Pub => Priv}}.
-key_pairs(S) -> maps:get(key_pairs, S, #{}).
+    set_account(Account2, Trees).
 
 set_account(Account, Trees) ->
     AccTree = aec_accounts_trees:enter(Account, aec_trees:accounts(Trees)),
     aec_trees:set_accounts(Trees, AccTree).
-
-trees(#{} = S) ->
-    maps:get(trees, S, aec_trees:new()).
 
 -define(BENEFICIARY_PUBKEY, <<12345:?BENEFICIARY_PUB_BYTES/unit:8>>).
 default_tx_env(Height) ->
