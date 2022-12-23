a90a622da4af6f6722497314ec9626639181c599
Gaith Hallak <gaithhallak@gmail.com>
2022-12-23 12:46:10 +0300

    Use get_ready_chain() to ban commands on breaks

--- src/aere_repl.erl
+++ src/aere_repl.erl
@@ -120,13 +120,6 @@ apply_command(skip, [], State) ->
     State;
 apply_command(reset, [], _) ->
     init_state();
-apply_command(help, Arg, State) ->
-    case Arg of
-        [On] -> {aere_msg:help(On), State};
-        _ -> {aere_msg:help(), State}
-    end;
-apply_command(print, [What], State) ->
-    {print_state(State, What), State};
 apply_command(type, I, State) ->
     Stmts = aere_sophia:parse_body(I),
     Contract = aere_mock:eval_contract(Stmts, State),
@@ -135,6 +128,41 @@ apply_command(type, I, State) ->
     TypeStr = aeso_ast_infer_types:pp_type("", Type),
     TypeStrClean = re:replace(TypeStr, ?TYPE_CONTAINER ++ "[0-9]*\\.", "", [global, {return, list}]),
     {aere_msg:output(TypeStrClean), State};
+apply_command(state, I, State) ->
+    get_ready_chain(State),
+    set_state(aere_sophia:parse_body(I), State);
+apply_command(eval, I, State) ->
+    get_ready_chain(State),
+    Parse = aere_sophia:parse_top(I),
+    case Parse of
+        {body, Body} ->
+            eval_expr(Body, State);
+        [{include, _, {string, _, Inc}}] ->
+            register_include(Inc, State);
+        [{letval, _, Pat, Expr}] ->
+            register_letval(Pat, Expr, State);
+        [{letfun, _, FName, Args, _, Body}] ->
+            register_letfun(FName, Args, Body, State);
+        [{type_def, _, Name, Args, Body}] ->
+            register_typedef(Name, Args, Body, State);
+        _ -> error(too_much_stuff) %% FIXME
+    end;
+apply_command(load, Modules, State) ->
+    get_ready_chain(State),
+    load_modules(Modules, State);
+apply_command(reload, [], State) ->
+    get_ready_chain(State),
+    reload_modules(State);
+apply_command(set, [Option|Args], State) ->
+    get_ready_chain(State),
+    set_option(list_to_atom(Option), Args, State);
+apply_command(help, Arg, State) ->
+    case Arg of
+        [On] -> {aere_msg:help(On), State};
+        _ -> {aere_msg:help(), State}
+    end;
+apply_command(print, [What], State) ->
+    {print_state(State, What), State};
 apply_command(disas, Args, State = #repl_state{blockchain_state = {breakpoint, ES}}) ->
     Chain = aefa_engine_state:chain_api(ES),
     apply_command(disas, Args, State#repl_state{blockchain_state = {ready, Chain}});
@@ -153,32 +181,7 @@ apply_command(continue, _, State = #repl_state{blockchain_state = BS}) ->
             eval_state(aefa_engine_state:set_breakpoint_stop(false, ES), State);
         _ ->
             {aere_msg:error("Not at breakpoint!"), State}
-    end;
-apply_command(_, _, State = #repl_state{blockchain_state = {breakpoint, _}}) ->
-    {aere_msg:output("Currently on a breakpoint"), State};
-apply_command(state, I, State) ->
-    set_state(aere_sophia:parse_body(I), State);
-apply_command(eval, I, State) ->
-    Parse = aere_sophia:parse_top(I),
-    case Parse of
-        {body, Body} ->
-            eval_expr(Body, State);
-        [{include, _, {string, _, Inc}}] ->
-            register_include(Inc, State);
-        [{letval, _, Pat, Expr}] ->
-            register_letval(Pat, Expr, State);
-        [{letfun, _, FName, Args, _, Body}] ->
-            register_letfun(FName, Args, Body, State);
-        [{type_def, _, Name, Args, Body}] ->
-            register_typedef(Name, Args, Body, State);
-        _ -> error(too_much_stuff) %% FIXME
-    end;
-apply_command(load, Modules, State) ->
-    load_modules(Modules, State);
-apply_command(reload, [], State) ->
-    reload_modules(State);
-apply_command(set, [Option|Args], State) ->
-    set_option(list_to_atom(Option), Args, State).
+    end.
 
 -spec set_state([aeso_syntax:stmt()], repl_state()) -> command_res().
 set_state(Body, S0) ->
