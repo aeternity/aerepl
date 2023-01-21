42655ea8cc9bfe2675b953d75eb850ce14bae01b
Gaith Hallak <gaithhallak@gmail.com>
2023-01-21 14:34:31 +0300

    Throw in both read_file and read_files

--- src/aere_utils.erl
+++ src/aere_utils.erl
@@ -4,26 +4,43 @@
         , read_files/1
         ]).
 
--spec read_files(FileNames) -> [{FileName, File}] | no_return()
-    when FileNames :: [FileName],
-         FileName  :: string(),
+
+-spec read_file(FileName) -> File | no_return()
+    when FileName :: string(),
+         File     :: binary().
+
+read_file(FileName) ->
+    case read_file_safe(FileName) of
+        {ok, File} ->
+            File;
+        {error, Err} ->
+            Failed = [{FileName, file:format_error(Err)}],
+            throw({repl_error, aere_msg:files_load_error(Failed)})
+    end.
+
+
+-spec read_files([FileName]) -> [{FileName, File}] | no_return()
+    when FileName  :: string(),
          File      :: binary().
+
 read_files(Filenames) ->
-    Files = [read_file(F) || F <- Filenames],
+    Files  = [read_file_safe(F) || F <- Filenames],
+    Failed = [{File, file:format_error(Err)} || {File, {error, Err}} <- lists:zip(Filenames, Files)],
+    case Failed of
+        [] -> lists:zip(Filenames, [File || {ok, File} <- Files]);
+        _  -> throw({repl_error, aere_msg:files_load_error(Failed)})
+    end.
 
-    case [{File, file:format_error(Err)} || {File, {error, Err}} <- lists:zip(Filenames, Files)] of
-        []     -> ok;
-        Failed -> throw({repl_error, aere_msg:files_load_error(Failed)})
-    end,
 
-    lists:zip(Filenames, [File || {ok, File} <- Files]).
-
-%% Reads files either from working dir or stdlib
--spec read_file(FileName) -> {ok, binary()} | {error, term()}
+-spec read_file_safe(FileName) -> {ok, binary()} | {error, term()}
     when FileName :: string().
-read_file(Filename) ->
+%% @doc
+%% Reads files either from working dir or stdlib.
+
+read_file_safe(Filename) ->
     case file:read_file(Filename) of
         {error, enoent} ->
             file:read_file(filename:join(aeso_stdlib:stdlib_include_path(), Filename));
-        Res -> Res
+        Res ->
+            Res
     end.
